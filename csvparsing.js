64022725423c89d7567f647bb2184e9758a7d45f
Ramyachinna <ramyamails94@gmail.com>
2017-05-23 16:03:19 +0530

    Update csvparsing.js

--- csvparsing.js
+++ csvparsing.js
@@ -1,19 +1,20 @@
-var csvFilePath ='./pets.csv'
+var csvFilePath = './pets.csv'
 var csv = require('csvtojson')
 global.data = [];
-function getMethod(callback){
-csv()
-.fromFile(csvFilePath)
-.on('json',(jsonObj)=>{
-	var value = jsonObj
-     data.push(value);
-	 
-})
-.on('done',(error)=>{
-	 callback(data);
-	 console.log('------------------------------------------------------>')
-     console.log('****The data inserted Sucessfully into the database****')
-	 console.log('<------------------------------------------------------')
-})
+
+function getMethod(callback) {
+    csv()
+        .fromFile(csvFilePath)
+        .on('json', (jsonObj) => {
+            var value = jsonObj
+            data.push(value);
+
+        })
+        .on('done', (error) => {
+            callback(data);
+            console.log('------------------------------------------------------>')
+            console.log('****The data inserted Sucessfully into the database****')
+            console.log('<------------------------------------------------------')
+        })
 }
 exports.getMethod = getMethod;
