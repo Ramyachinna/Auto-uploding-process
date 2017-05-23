9dcb663048e01f60e56f511e77b20e1d42a28cf4
Ramyachinna <ramyamails94@gmail.com>
2017-05-23 16:05:50 +0530

    Update mapping.js

--- mapping.js
+++ mapping.js
@@ -3,26 +3,25 @@ var properties = PropertiesReader('./import.properties');
 var parsing = require('./csvparsing');
 
 
-function mappingMethod(data){
-	 
-	
-        if((data.type == "dog" ) | (data.type == "cat") | (data.type == "horse")){
-          data.type = properties.get(data.type);
-		 }
-		if((data.sex == "male") | (data.sex == "female")){
-		  data.sex = properties.get(data.sex);
-		}
-		if((data.size == "Small") | (data.size == "Medium") | (data.size == "Large")){
-		  data.size = properties.get(data.size);
-		}
-		if((data.status == "InHouse") | (data.status == "Homestay")){
-		  //console.log(data.status)
-		  data.status = properties.get(data.status);
-		}
-	return data;
- 
- };
- 
-exports.mappingMethod = mappingMethod;
+function mappingMethod(data) {
 
+
+    if ((data.type == "dog") | (data.type == "cat") | (data.type == "horse")) {
+        data.type = properties.get(data.type);
+    }
+    if ((data.sex == "male") | (data.sex == "female")) {
+        data.sex = properties.get(data.sex);
+    }
+    if ((data.size == "Small") | (data.size == "Medium") | (data.size == "Large")) {
+        data.size = properties.get(data.size);
+    }
+    if ((data.status == "InHouse") | (data.status == "Homestay")) {
+        //console.log(data.status)
+        data.status = properties.get(data.status);
+    }
+    return data;
+
+};
+
+exports.mappingMethod = mappingMethod;
 
